ed0eb62ac22f60dc71ba724dc19b7fdbf139438a
hubert-lasota <hxprogramming@gmail.com>
2024-05-06 18:07:03 +0200

    initial commit

---
+++ src/main/java/org/hl/socialspherebackend/SocialSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hl.socialspherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSphereBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hl/socialspherebackend/SocialSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hl.socialspherebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
